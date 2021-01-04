2f0aea2f6fc9c817b2918813e996715abe6e2cc9
rpcavaco <rpcavaco@gmail.com>
2021-01-04 09:37:07 +0000

    First tests

--- graphinet/graphinet.py
+++ graphinet/graphinet.py
@@ -1,32 +1,26 @@
  
 from queue import PriorityQueue
 
-from typing import Optional, List
+from typing import Optional, List, Union
 
 PARENT = 0
 CHILD = 2
 
-# class TreeError(RuntimeError):
-	# def __init__(self, p_msg):
-		# self.msg = p_msg
-	# def __str__(self):
-		# return f"TreeError: {self.msg}"
-		
 class NotBaseNodeError(RuntimeError):
 	def __str__(self):
-		return "Not a _NodeSkel/_BaseNode instance"
+		return "Not a BaseNode instance"
 
-class NodeAlreadyExists(RuntimeError):
-	def __init__(self, p_thisnode, p_othernode, p_reltype: int):
-		self.thisid = p_thisnode.id
-		self.otherid = p_othernode.id
-		self.reltype = p_reltype
-	def __str__(self):
-		if self.reltype == PARENT:
-			ret = f"Node {self.otherid} already marked as parent of id: {self.thisid}"
-		else:
-			ret = f"Node {self.otherid} already marked as child of id: {self.thisid}"
-		return ret
+# class NodeAlreadyExists(RuntimeError):
+# 	def __init__(self, p_thisnode, p_othernode, p_reltype: int):
+# 		self.thisid = p_thisnode.ident
+# 		self.otherid = p_othernode.ident
+# 		self.reltype = p_reltype
+# 	def __str__(self):
+# 		if self.reltype == PARENT:
+# 			ret = f"Node {self.otherid} already marked as parent of id: {self.thisid}"
+# 		else:
+# 			ret = f"Node {self.otherid} already marked as child of id: {self.thisid}"
+# 		return ret
 
 class MissingNodeIDsError(RuntimeError):
 	def __init__(self, p_ids):
@@ -45,82 +39,109 @@ class CycleAttemptError(RuntimeError):
 		self.p_ids = p_ids
 	def __str__(self):
 		return f"Attempt to insert cycle on ids: {self.p_ids}"
-				
+
+class SelfReferenceAttenpt(RuntimeError):
+	def __init__(self, p_id):
+		self.p_ids = p_id
+	def __str__(self):
+		return f"Attempt to self reference, id: {self.p_ids}"
+
 class ImproperSortingMethod(RuntimeError):
 	def __init__(self, p_classname):
 		self.p_classname = p_classname
 	def __str__(self):
 		return f"Method __lt__ for sorting nodes is improperly extended for {self.p_classname}"
 		
-class _NodeSkel(object):	
-	def __init__(self):
-		self.parentids = None
-		self.childrenids = None
-		self.id = None
-		
-class BaseMeshNode(_NodeSkel):
+class BaseGraphNode(object):
 
-	def __init__(self, ident=None, parentids: Optional[List[_NodeSkel]] = [], childrenids: Optional[List[_NodeSkel]] = []):
-		self.parentids = parentids
-		self.childrenids = childrenids
+	def __init__(self, ident: Optional[List[Union[str,int]]] = None, 
+			parentids: Optional[List[Union[str,int]]] = None, 
+			childrenids: Optional[List[Union[str,int]]] = None):
 		if ident is None:
-			self.id = str(id(self))
+			self.ident = str(id(self))
 		else:
-			self.id = ident
-			
+			self.ident = ident
+		if parentids is None:
+			self.parentids = []
+		else:
+			self.parentids = parentids
+		if childrenids is None:
+			self.childrenids = []
+		else:
+			self.childrenids = parentids
+
 	def __repr__(self):
-		return self.id
+		return str(self.ident)
 		
-	def __eq__(self, other):
-		return self.id == other.id
+	def __eq__(self, p_other):
+		if not isinstance(p_other, BaseGraphNode):
+			raise NotBaseNodeError()
+		return self.ident == p_other.ident
 		
-	def __ne__(self, other):
-		return self.id != other.id
+	def __ne__(self, p_other):
+		if not isinstance(p_other, BaseGraphNode):
+			raise NotBaseNodeError()
+		return self.ident != p_other.ident
 		
-	def __lt__(self, other):
-		"""THIS METHOD SHUOLD BE EXTENDED BY SUBCLASSES, TO PROPER SORT NODES ALONG MESH"""
-		return self.id < other.id
-			
-	def getParentIds(self) -> List[str]:
+	def __lt__(self, p_other):
+		"""THIS METHOD SHUOLD BE EXTENDED BY SUBCLASSES, TO PROPER SORT NODES ALONG Graph"""
+		if not isinstance(p_other, BaseGraphNode):
+			raise NotBaseNodeError()
+		return self.ident < p_other.ident
+
+	def getParentIds(self) -> List[Union[str,int]]:
 		return self.parentids
 
-	def getChildIds(self) -> List[str]:
+	def getChildrenIds(self) -> List[Union[str,int]]:
 		return self.childrenids
-		
-	def checkParentExists(self, other: _NodeSkel):
-		if other is None or not isinstance(other, _NodeSkel):
-			raise NotBaseNodeError()			
-		return other.id in self.getParentIds()
-		
-	def checkChidExists(self, other: _NodeSkel):
-		if other is None or not isinstance(other, _NodeSkel):
-			raise NotBaseNodeError()			
-		return other.id in self.getChildIds()
-			
-	def addParent(self, other: _NodeSkel):
-		if self.checkParentExists(other):
-			raise NodeAlreadyExists(self, other, PARENT)
-		other.addChild(self)
-		self.parentids.append(other.id)
 
-	def addChild(self, other: _NodeSkel):
-		if self.checkChidExists(other):
-			raise NodeAlreadyExists(self, other, CHILD)
-		other.addParent(self)
-		self.childrenids.append(other.id)
-			
-	def addChildren(self, children: List[_NodeSkel]):
-		for chld in children:
-			self.addChild(chld)
-			
+	def removeParentId(self, p_pid: Union[str,int]):
+		if p_pid in self.parentids:
+			self.parentids.remove(p_pid)
 
-class DirectedMesh(object):	
+	def removeChildId(self, p_cid: Union[str,int]):
+		if p_cid in self.childrenids:
+			self.childrenids.remove(p_cid)
+
+	def assertOtherIsParent(self, p_other):
+		if not isinstance(p_other, BaseGraphNode):
+			raise NotBaseNodeError()
+		if not p_other.ident in self.getParentIds():
+			self.parentids.append(p_other.ident)
+		if not self.ident in p_other.getChildrenIds():
+			p_other.childrenids.append(self.ident)
+
+	def assertOtherIsChild(self, p_other):
+		if not isinstance(p_other, BaseGraphNode):
+			raise NotBaseNodeError()
+		if not p_other.ident in self.getChildrenIds():
+			self.childrenids.append(p_other.ident)
+		if not self.ident in p_other.getParentIds():
+			p_other.parentids.append(self.ident)
+
+	# def removeOtherFromChildren(self, p_other):
+	# 	if not isinstance(p_other, BaseGraphNode):
+	# 		raise NotBaseNodeError()
+	# 	if p_other.ident in self.getChildrenIds():
+	# 		self.childrenids.remove(p_other.ident)
+	# 	if self.ident in p_other.getParentIds():
+	# 		p_other.parentids.remove(self.ident)
+
+	# def removeOtherFromParents(self, p_other):
+	# 	if not isinstance(p_other, BaseGraphNode):
+	# 		raise NotBaseNodeError()
+	# 	if p_other.ident in self.getParentIds():
+	# 		self.parentids.remove(p_other.ident)
+	# 	if self.ident in p_other.getChildrenIds():
+	# 		p_other.childrenids.remove(self.ident)
+
+class DirectedAciclicGraph(object):	
 	
 	def __init__(self):
-		self.rootids = []		
+		self.rootids: List[Union[str,int]] = []		
 		self.nodes = {}
 		
-	def checkIDs(self, lids: List[str]):
+	def checkIDs(self, lids: List[Union[str,int]]) -> None:
 		if len(lids) < 1:
 			return
 		tstids = set(lids)
@@ -128,20 +149,36 @@ class DirectedMesh(object):
 		if len(xstkeys) > 0 and tstids.isdisjoint(xstkeys):
 			raise MissingNodeIDsError(tstids.difference(xstkeys))	
 			
-	def iterateUp(self, start_ident):
+	def iterateUp(self, start_ident: Optional[Union[str,int]] = None, 
+			parentids: Optional[List[Union[str,int]]] = None) -> None:
+
+		assert not start_ident is None or \
+			not parentids is None
 		
-		if not start_ident in self.nodes.keys():
+		if not start_ident is None and not start_ident in self.nodes.keys():
 			raise MissingNodeIDsError(start_ident)
 			
 		fringe = PriorityQueue()
 		fringed_ids = set()
 		lvl = 0
+
+		if not start_ident is None:
+			
+			n = self.nodes[start_ident]
+			fringe.put((lvl, n))
+			lvl += 1
 		
-		n = self.nodes[start_ident]
-		for pid in n.parentids:
-			p = self.nodes[pid]
-			fringe.put((lvl, p))
-			fringed_ids.add(pid)
+			for pid in n.parentids:
+				p = self.nodes[pid]
+				fringe.put((lvl, p))
+				fringed_ids.add(pid)
+
+		else:
+
+			for pid in parentids:
+				p = self.nodes[pid]
+				fringe.put((lvl, p))
+				fringed_ids.add(pid)
 			
 		while not fringe.empty():
 			flvl, fn = fringe.get()
@@ -155,22 +192,38 @@ class DirectedMesh(object):
 					fringed_ids.add(fpid)
 				except TypeError:
 					raise ImproperSortingMethod(str(type(fp)))
-		
-	def iterateDown(self, start_ident):
-		
-		if not start_ident in self.nodes.keys():
+
+	def iterateDown(self, start_ident: Optional[Union[str,int]] = None, 
+			childrenids: Optional[List[Union[str,int]]] = None) -> None:
+
+		assert not start_ident is None or \
+			not childrenids is None
+
+		if not start_ident is None and not start_ident in self.nodes.keys():
 			raise MissingNodeIDsError(start_ident)
 			
 		fringe = PriorityQueue()
 		fringed_ids = set()
 		lvl = 0
-		
-		n = self.nodes[start_ident]
-		for pid in n.childrenids:
-			p = self.nodes[pid]
-			fringe.put((lvl, p))
-			fringed_ids.add(pid)
+
+		if not start_ident is None:
 			
+			n = self.nodes[start_ident]
+			fringe.put((lvl, n))
+			lvl += 1
+			
+			for pid in n.childrenids:
+				p = self.nodes[pid]
+				fringe.put((lvl, p))
+				fringed_ids.add(pid)
+
+		else:
+
+			for pid in childrenids:
+				p = self.nodes[pid]
+				fringe.put((lvl, p))
+				fringed_ids.add(pid)
+
 		while not fringe.empty():
 			flvl, fn = fringe.get()
 			yield fn
@@ -184,48 +237,139 @@ class DirectedMesh(object):
 				except TypeError:
 					raise ImproperSortingMethod(str(type(fp)))
 		
-	def addNode(self, node: BaseMeshNode):	
-		
-		if node.id in self.nodes.keys():
-			raise ExistingNodeIdError(node.id)
-			
-		self.checkIDs(node.parentids)
-		self.checkIDs(node.childrenids)
-		
-		#lparents = [self.nodes[pid] for pid in node.parentids]
-		#lchildren = [self.nodes[pid] for pid in node.childrenids]
+	def addNode(self, p_node: BaseGraphNode, doraise: Optional[bool] = False) -> BaseGraphNode:	
 
-		self.nodes[node.id] = node
-		
-		if len(node.parentids) == 0:
-			# New root
-			if len(node.childrenids) > 0:
-				roots_to_cancel = set(node.childrenids).intersection(set(self.rootids))
+		if not isinstance(p_node, BaseGraphNode):
+			raise NotBaseNodeError()
+
+		if p_node.ident in self.nodes.keys():
+			raise ExistingNodeIdError(p_node.ident)
+
+		chldids = set(p_node.getChildrenIds())
+		parids = set(p_node.getParentIds())
+
+		if p_node.ident in chldids or p_node.ident in parids:
+			raise SelfReferenceAttenpt(p_node.ident)
+
+		cycle_alarm_ids = set()
+		# prevent cycles: upward
+		iup = {n.ident for n in self.iterateUp(parentids = parids)}
+		if len(chldids) > 0 and len(iup) > 0 and chldids.issubset(iup):
+			for xcid in chldids.intersection(iup):
+				cycle_alarm_ids.add(xcid)
+				p_node.removeChildId(xcid)
+
+		# prevent cycles: downward
+		idn = {n.ident for n in self.iterateDown(childrenids=chldids)}
+		if len(parids) > 0 and len(idn) > 0 and parids.issubset(idn):
+			for xpid in parids.intersection(idn):
+				cycle_alarm_ids.add(xpid)
+				p_node.removeParentId(xpid)
+
+		if doraise and len(cycle_alarm_ids) > 0:
+			raise CycleAttemptError(cycle_alarm_ids)
+
+		chldids = set(p_node.getChildrenIds())
+		parids = set(p_node.getParentIds())
+
+		self.checkIDs(parids)
+		self.checkIDs(chldids)
+
+		lparents = [self.nodes[pid] for pid in parids]
+		lchildren = [self.nodes[cid] for cid in chldids]
+
+		if len(lparents) > 0:
+			for parnode in lparents:
+				p_node.assertOtherIsParent(parnode)
+		else:
+			if len(chldids) > 0:
+				roots_to_cancel = chldids.intersection(set(self.rootids))
 				for rtc in roots_to_cancel:
 					self.rootids.remove(rtc)
-			self.rootids.append(node.id)
-			
-		sa = {cid for cid in self.nodes[node.id].childrenids}
-		sb = {n.id for n in self.iterateUp(node.id)}
-		if len(sa) > 0 and len(sb) > 0 and sa.issubset(sb):
-			raise CycleAttemptError(sa.intersection(sb))
+			self.rootids.append(p_node.ident)
 
-		sa = {pid for pid in self.nodes[node.id].parentids}
-		sb = {n.id for n in self.iterateDown(node.id)}
-		if len(sa) > 0 and len(sb) > 0 and sa.issubset(sb):
-			raise CycleAttemptError(sa.intersection(sb))
+		if len(lchildren) > 0:
+			for chldnode in lchildren:
+				p_node.assertOtherIsChild(chldnode)
+
+		self.nodes[p_node.ident] = p_node
 		
-		return self.nodes[node.id]
+		return self.nodes[p_node.ident]
+
+	def getNode(self, p_ident: str, doraise: Optional[bool] = False) -> Union[None, BaseGraphNode]:
+
+		if p_ident is None or not p_ident in self.nodes.keys():
+			if doraise:
+				raise MissingNodeIDsError(p_ident)
+			ret = None
+		else:
+			ret = self.nodes[p_ident]
+
+		return ret
+
+	def addEdge(self, p_fromid: Union[str,int], p_toid: Union[str,int], doraise: Optional[bool] = False) -> Union[None,BaseGraphNode]:
+
+		ret = None
+
+		if not p_fromid in self.nodes.keys():
+			raise MissingNodeIDsError(p_fromid)
+		if not p_toid in self.nodes.keys():
+			raise MissingNodeIDsError(p_toid)
+
+		if p_fromid == p_toid:
+			raise SelfReferenceAttenpt(p_fromid)
+
+		# prevent cycles: upward
+		cycle_alarm_ids = set()
+		for iid in self.iterateUp(start_ident=p_fromid):
+			if p_toid == iid.ident:
+				cycle_alarm_ids.add(p_toid)
+				break
+
+		for iid in self.iterateDown(start_ident=p_toid):
+			if p_fromid == iid.ident:
+				cycle_alarm_ids.add(p_fromid)
+				break
+
+		if len(cycle_alarm_ids) > 0:
+			if doraise:
+				raise CycleAttemptError(cycle_alarm_ids)
+		else:
+			fnd = self.nodes[p_fromid]
+			tnd = self.nodes[p_toid]
+			fnd.assertOtherIsChild(tnd)
+			ret = fnd
+
+		return ret
+		
 		
 
-if __name__ == "__main__":
-	m = DirectedMesh()
-	m.addNode(BaseMeshNode(ident="zeroot"))
-	m.addNode(BaseMeshNode(ident="zefilhoa", parentids=["zeroot"]))
-	m.addNode(BaseMeshNode(ident="zefilhob", parentids=["zeroot"]))
-	m.addNode(BaseMeshNode(ident="zenetob", parentids=["zefilhoa", "zefilhob"]))
-	m.addNode(BaseMeshNode(ident="zbisenetob", parentids=["zenetob"]))
-	m.addNode(BaseMeshNode(ident="zbisenetoc", parentids=["zenetob"]))
+def main():
+	print("-A---------------------------------------")
+	m = DirectedAciclicGraph()
+	m.addNode(BaseGraphNode(ident="zeroot"))
+	m.addNode(BaseGraphNode(ident="zefilhoa", parentids=["zeroot"]))
+	m.addNode(BaseGraphNode(ident="zefilhob", parentids=["zeroot"]))
+	m.addNode(BaseGraphNode(ident="zenetob", parentids=["zefilhoa", "zefilhob"]))
+	m.addNode(BaseGraphNode(ident="zbisenetob", parentids=["zenetob"]))
+	m.addNode(BaseGraphNode(ident="zbisenetoc", parentids=["zenetob"]))
 	print("Root ids:", m.rootids)
 	print("Nodes:",m.nodes.keys())
-	print("Iterate:",list(m.iterateUp("zbisenetoc")))
+	print("Iterate up zbisenetob:",  list(m.iterateUp("zbisenetob")))
+	print("Iterate down zeroot:",list(m.iterateDown("zeroot")))
+	print("-B---------------------------------------")
+	for k, v in m.nodes.items():
+		print(k , v.__dict__)
+	print("-C---------------------------------------")
+	m.addEdge("zeroot", "zenetob", doraise=True)
+	for k, v in m.nodes.items():
+		print(k , v.__dict__)
+	print("Iterate up zbisenetob 2nd:",  list(m.iterateUp("zbisenetob")))
+	print("-D---------------------------------------")
+	m.addEdge("zenetob", "zeroot", doraise=True)
+
+
+
+if __name__ == "__main__":
+	main()
+
---
+++ test/test_basic.py
@@ -0,0 +1,63 @@
+
+import pytest
+
+from graphinet.graphinet import DirectedAciclicGraph, BaseGraphNode, CycleAttemptError, MissingNodeIDsError
+
+@pytest.fixture()
+def prepared_dag():
+	m = DirectedAciclicGraph()
+	m.addNode(BaseGraphNode(ident="zeroot"))
+	m.addNode(BaseGraphNode(ident="zefilhoa", parentids=["zeroot"]))
+	m.addNode(BaseGraphNode(ident="zefilhob", parentids=["zeroot"]))
+	m.addNode(BaseGraphNode(ident="zenetob", parentids=["zefilhoa", "zefilhob"]))
+	m.addNode(BaseGraphNode(ident="zbisenetob", parentids=["zenetob"]))
+	m.addNode(BaseGraphNode(ident="zbisenetoc", parentids=["zenetob"]))
+	yield m
+
+class TestClass:
+
+	def test_getnode(self, prepared_dag):
+		with pytest.raises(MissingNodeIDsError) as e_info:
+			prepared_dag.getNode("xafs", doraise=True)
+		assert prepared_dag.getNode("xifs") == None
+		assert isinstance(prepared_dag.getNode("zenetob"), BaseGraphNode)
+
+	def test_nodeslist(self, prepared_dag):
+		assert len(list(prepared_dag.nodes.keys())) == 6
+
+	def test_root(self, prepared_dag):
+		assert prepared_dag.rootids == ['zeroot']
+
+	def test_nodes(self, prepared_dag):
+		assert len(prepared_dag.getNode("zeroot").getParentIds()) == 0
+		assert set(prepared_dag.getNode("zeroot").getChildrenIds()) == set(['zefilhoa', 'zefilhob'])
+
+		assert prepared_dag.getNode("zefilhoa").getParentIds() == ["zeroot"]
+		assert prepared_dag.getNode("zefilhoa").getChildrenIds() == ["zenetob"]
+
+		assert prepared_dag.getNode("zefilhob").getParentIds() == ["zeroot"]
+		assert prepared_dag.getNode("zefilhob").getChildrenIds() == ["zenetob"]
+
+		assert set(prepared_dag.getNode("zenetob").getParentIds()) == set(['zefilhoa', 'zefilhob'])
+		assert set(prepared_dag.getNode("zenetob").getChildrenIds()) == set(['zbisenetob', 'zbisenetoc'])
+
+		assert prepared_dag.getNode("zbisenetob").getParentIds() == ['zenetob']
+		assert len(prepared_dag.getNode("zbisenetob").getChildrenIds()) == 0
+
+		assert prepared_dag.getNode("zbisenetoc").getParentIds() == ['zenetob']
+		assert len(prepared_dag.getNode("zbisenetoc").getChildrenIds()) == 0
+
+	def test_itup(self, prepared_dag):
+		assert {n.ident for n in prepared_dag.iterateUp("zbisenetob")} == set(['zbisenetob', 'zenetob', 'zefilhoa', 'zefilhob', 'zeroot'])
+
+	def test_itdwn(self, prepared_dag):
+		assert {n.ident for n in prepared_dag.iterateDown("zeroot")} == set(['zeroot', 'zefilhoa', 'zefilhob', 'zenetob', 'zbisenetob', 'zbisenetoc'])
+
+	def test_edge1(self, prepared_dag):
+		prepared_dag.addEdge("zeroot", "zenetob", doraise=True)
+		assert {n.ident for n in prepared_dag.iterateUp("zbisenetob")} == set(['zbisenetob', 'zenetob', 'zefilhoa', 'zefilhob', 'zeroot'])
+		assert set(prepared_dag.getNode("zenetob").getParentIds()) == set(['zeroot','zefilhoa', 'zefilhob'])
+
+	def test_edge2(self, prepared_dag):
+		with pytest.raises(CycleAttemptError) as e_info:
+			prepared_dag.addEdge("zenetob", "zeroot", doraise=True)
